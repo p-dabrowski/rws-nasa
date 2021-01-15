b22eea2d5e63c7198696ed7261be9fe10a3c1fdd
Przemysław Dąbrowski <przemyslaw.dabrowski@rst.com.pl>
2021-01-15 12:16:46 +0100

    readme update

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# BUILD
+# NASA API requests app
+This simple app performs some calls on Nasa Api, retrieves 5 images with Mars Surface and also checks video files location.
+
+## Technologies
+Java, RestAssured, json.org (jsons decoder/encoder)
+
+## BUILD
 mvn install
 
-# RUN
+## RUN
 mvn exec:java -Dexec.mainClass="org.example.NasaApi"
--- src/main/java/org/example/NasaClient.java
+++ src/main/java/org/example/NasaClient.java
@@ -11,9 +11,9 @@ public class NasaClient {
 
     private List<Object> getImageUrls(Map<String, String> queryParams) {
         String baseUri = "https://images-api.nasa.gov";
-        String basePath = "search";
+        String searchBasePath = "search";
 
-        Response response = new HttpClient().sendRequest(baseUri, basePath, queryParams);
+        Response response = new HttpClient().sendRequest(baseUri, searchBasePath, queryParams);
 
         List<Object> urls = response.jsonPath().getList("collection.items.links.href");
 
@@ -21,7 +21,8 @@ public class NasaClient {
     }
 
     public List<Object> getMarsImagesUrls() {
-        Map<String, String> queryParams = new HashMap<String,String>();
+        Map<String, String> queryParams = new HashMap<String, String>();
+
         queryParams.put("q", "mars");
         queryParams.put("year_start", "2018");
         queryParams.put("year_end", "2018");
@@ -32,7 +33,7 @@ public class NasaClient {
     }
 
     public List<Object> getVideoUrls() {
-        Map<String, String> queryParams = new HashMap<String,String>();
+        Map<String, String> queryParams = new HashMap<String, String>();
 
         queryParams.put("year_start", "2018");
         queryParams.put("year_end", "2018");
